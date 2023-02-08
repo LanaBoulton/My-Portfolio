b7e440f7b37e15813868f604b27da199a7175c14
Lana <gavrylovalova@gmail.com>
2023-02-07 22:22:03 -0800

    responsive adjustments

---
+++ src/myPortfolio.js
@@ -0,0 +1,8 @@
+const hambrgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hambrgerButton.addEventListener("click", toggleButton)
